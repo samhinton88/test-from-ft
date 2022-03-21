dd52b140488369448f986c69a0b2879889367238
samhinton88 <sam.hinton88@gmail.com>
2022-03-21 13:49:38 +0000

    left nav

---
+++ components/loading-spinner/left-nav/component.tsx
@@ -0,0 +1,7 @@
+import { useState } from 'react';
+
+import styles from './left-nav.module.css';
+
+export const LeftNav = () => {
+  return <div className={styles.leftNav}>left-nav</div>;
+};
---
+++ components/loading-spinner/left-nav/left-nav.test.tsx
@@ -0,0 +1,19 @@
+
+import { render, fireEvent, waitFor, screen } from '@testing-library/react'
+import { LeftNav } from "./component"
+
+const getMock = jest.fn();
+
+
+
+
+describe("<LeftNav />", () => {
+  
+});
+
+
+
+
+
+
+
