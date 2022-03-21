3c8023b49b92620080b510ad059cd5a758101608
samhinton88 <sam.hinton88@gmail.com>
2022-03-21 20:24:21 +0000

    Add files via upload

---
+++ components/component.js
@@ -0,0 +1,10 @@
+import { useState } from 'react';
+
+export const Dasboard = () => {
+  const [searchValue, setSearchValue] =
+    useState();
+  const [isLoading, setIsLoading] =
+    useState();
+
+  return <div>dasboard</div>;
+};
